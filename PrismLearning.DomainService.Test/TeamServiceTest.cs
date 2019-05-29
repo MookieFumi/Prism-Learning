6b0ffad1f3ec7311a2216bafb8a2bf5557460d2a
Aitor Ballesteros <aitor.ballesteros@cognizant.com>
2019-05-29 11:59:29 +0200

    Added new assembly for testing

---
+++ PrismLearning.DomainService.Test/TeamServiceTest.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PrismLearning.DomainService.Test
+{
+    public class TeamServiceTest
+    {
+
+        [Fact]
+        public async Task GetTeams_Should_Not_Return_Null()
+        {
+            var sut = new TeamsService();
+
+            var teams = await sut.GetTeams();
+
+            Assert.NotNull(teams);
+        }
+    }
+}
