8a15ae35cb90a9b3c1bc5a84d09b00b9dc9adf4c
Aitor Ballesteros <aitor.ballesteros@cognizant.com>
2019-05-29 10:12:18 +0200

    Added BindableLayout

---
+++ PrismLearning/Controls/TeamPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PrismLearning.Controls
+{
+    public partial class TeamPanel : ContentPage
+    {
+        public TeamPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
